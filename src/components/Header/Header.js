86d97b0497700bcf3f4006be5da5244a91535827
Dawid Brzeski <daw.brzeski@gmail.com>
2022-02-03 00:43:04 +0100

    Profile Photo from Google

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Search as SearchIcon } from '@mui/icons-material';
 import { HelpOutline as HelpMark } from '@mui/icons-material';
 import {HeaderContainer, HeaderLeft, HeaderAvatar, HeaderTimeIcon, HeaderSearch, HeaderRight} from "./styled";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase';
 
 
 function Header() {
+
+  const [user] = useAuthState(auth);
   return (
     <HeaderContainer>
 
       <HeaderLeft>
-        <HeaderAvatar />
+        <HeaderAvatar 
+        alt={user?.displayName}
+        src={user?.photoURL}
+        onClick={() => auth.signOut()}
+        />
         <HeaderTimeIcon />
       </HeaderLeft>
 
