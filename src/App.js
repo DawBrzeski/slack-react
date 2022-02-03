e8811804f1817e2166985cd89ffd2fdcf57e6b3e
Dawid Brzeski <daw.brzeski@gmail.com>
2022-02-03 01:05:52 +0100

    loading page added

--- src/App.js
+++ src/App.js
@@ -12,8 +12,28 @@ import Chat from './components/Chat/Chat';
 import { auth } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import Login from './components/Login/Login';
+import Spinner from "react-spinkit";
+
+
+
 function App() {
   const [user, loading] = useAuthState(auth)
+
+if (loading) {
+  return (
+    <AppLoading>
+      <AppLoadingContent>
+      <img src="https://cdn.worldvectorlogo.com/logos/slack-1.svg" alt="logo" />
+      <Spinner
+      name="ball-spin-fade-loader"
+      color="purple"
+      fadeIn="none"
+
+      />
+      </AppLoadingContent>
+    </AppLoading>
+  )
+}
 
   return (
     <div className="App">
@@ -49,3 +69,25 @@ display: flex;
 height: 100vh; 
 `;
 
+const AppLoading = styled.div`
+display: grid;
+place-items: center; 
+height: 100vh;
+width: 100%;
+
+`;
+
+const AppLoadingContent = styled.div`
+text-align:  center;
+padding-bottom: 100px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+> img {
+  height: 100px;
+  padding: 20px; 
+  margin-bottom: 40px;
+}
+`;
